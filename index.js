70d2fd2d813e336ed08d2af3f36ae09018d4f6b9
kehinde ojo <ojok761@gmail.com>
2023-10-24 12:09:24 -0400

    edited some files

--- index.js
+++ index.js
@@ -1,8 +1,72 @@
 // Start.onclick= Start
 let lastTurn;
 let powerUp
-
 let maintainSpeed;
+// this is for the tesla
+function turnning(){
+  if (powerUp.toLowerCase() === "a") {
+    alert("you got an handling upgrade ");
+     lastTurn = prompt("you are at the last turn do you A. maintain your speed B. speed up C. slow down  ")
+    if (lastTurn.toLowerCase() === "a") {
+      alert("you passed everyone and you won the race")
+    } else if (lastTurn.toLowerCase() === "b") {
+      alert("you crashed lost the game ")
+    } else if (lastTurn.toLowerCase() === "c") {
+      alert("unfortunately you lost some speed and finshed the game last ")
+
+  }
+  }else if (powerUp.toLowerCase()==="b"){
+    alert("you have crashed please try again ")
+  }
+}
+function sameSpeed (){
+  if (maintainSpeed.toLowerCase()==="a"){
+    alert("your are going too fast and you crash ")
+
+  }else if (maintainSpeed.toLowerCase()==="b"){
+    alert("you got through the turn and you are first place and you won the race  ")
+
+  }else if (maintainSpeed.toLowerCase() ==="c")
+  {
+
+    alert("you made through the turn and you won the race ")
+  }
+  
+
+}
+// this is fot the toyota\
+function toyotaturnning(){
+  if (powerUp.toLowercase() === "a") {
+    alert("you got an handling upgrade ");
+     lastTurn = prompt("you are at the last turn do you A. maintain your speed B. speed up C. slow down  ")
+    if (lastTurn.toLowerCase() === "a") {
+      alert("you completed the turning but unfortunatly you ended the game at 4th because you are too slow")
+    } else if (lastTurn.toLowerCase() === "b") {
+      alert("you completed the turning and won the game")
+    } else if (lastTurn.toLowerCase() === "c") {
+      alert("  you completed the turning but unfortunatly you ended the game at 3rd")
+  
+  
+  }
+}else if (powerUp.toLowerCase()==="b"){
+  alert("you landed in front of the turning and won the game  ")
+}
+}
+function ToyotasameSpeed (){
+  if (maintainSpeed.toLowerCase()==="a"){
+    alert("you made it throught the turn and won the game  ")
+
+  }else if (maintainSpeed.toLowerCase()==="b"){
+    alert("you got through the turn but you finished the race at 4th  ")
+
+  }else if (maintainSpeed.toLowerCase() ==="c"){
+
+    alert("you made through the turn  but you finshed the race as 2nd  ")
+  }
+
+  
+
+}
 
 function Start() {
   const gameSelection = prompt(
@@ -16,17 +80,18 @@ function Start() {
       // let output = document.getElementById("output");
 
       // console.log(output);
+      // output.innerHTML= "the tesla is a very fast bike but has a bad handling goodluck. the match starts in 3 2 1"
       alert("the tesla is a very fast bike but has a bad handling goodluck. the match starts in 3 2 1 ");
 
       let line;
-      line = prompt("the race started and you are 2rd place. There is a  turning coming up, do you want to a. reduce speed b. keep the same speed c. speed up  ");
+      line = setTimeout(prompt("the race started and you are 2rd place. There is a  turning coming up, do you want to a. reduce speed b. keep the same speed c. speed up  "),5000);
 
       if (line.toLowerCase() === "a") {
         alert(
           "Your speed drop you are now in 4th place but you can still win this "
         )
          powerUp = prompt("wow there is a power up on the road do you a. pick it up b. miss it to get on a ramp ")
-          if (powerUp=== "a"|| "b"){
+          if (powerUp.toLowerCase()=== "a"|| "b"){
             turnning()
           }else{
             turnning()
@@ -35,10 +100,10 @@ function Start() {
       }else if (line.toLowerCase() ==="b"){
         alert("your bike was a little wobly but u manage to get it on track and mantain the 2nd place ")
        let  maintainSpeedPowerups =prompt("there is a power up on the road do you A. pick it up B. miss it and go on a ramp")
-       if (maintainSpeedPowerups==="a"){
+       if (maintainSpeedPowerups.toLowerCase()==="a"){
         alert("your bike get more speed and better handling ")
         maintainSpeed= prompt("you are ariving at a turn what do you do A. speed up b. slow down C maintain speed  ")
-        if (maintainSpeed=== "a"|| "b" ||"c"){
+        if (maintainSpeed.toLowerCase()=== "a"|| "b" ||"c"){
 
             sameSpeed();
 
@@ -63,7 +128,7 @@ function Start() {
           "Your speed drop you are now in 5th place but you can still win this "
         )
          powerUp = prompt("wow there is a power up on the road do you a. pick it up b. miss it to get on a ramp ")
-          if (powerUp=== "a"|| "b"){
+          if (powerUp.toLowerCase()=== "a"|| "b"){
             toyotaturnning();
           }else{
             toyotaturnning();
@@ -72,7 +137,7 @@ function Start() {
       }else if (mortocycleline ==="b"){
         alert("youmade it through the turn and u are at 3rd place ")
        powerUp =prompt("there is a power up on the road do you A. pick it up B. miss it and go on a ramp")
-       if (powerUp==="a"){
+       if (powerUp.toLowerCase()==="a"){
         alert("your bike get more speed and more handling ")
         maintainSpeed= prompt("you are ariving at a turn what do you do A. speed up b. slow down C maintain speed  ")
         if (maintainSpeed=== "a"|| "b"){
@@ -84,79 +149,14 @@ function Start() {
         }
        
       
-      }else if (mortocycleline ==="c"){
+      }else if (mortocycleline.toLowerCase() ==="c"){
         alert("you passed everyone and won the game ")
 
       }
     }
   } 
-  }else if (gameSelection==="b"){
+  }else if (gameSelection.toLowerCase()==="b"){
     
-  alert("fuck you")
+ 
 }
-}
-// this is for the tesla
-function turnning(){
-  if (powerUp === "a") {
-    alert("you got an handling upgrade ");
-     lastTurn = prompt("you are at the last turn do you A. maintain your speed B. speed up C. slow down  ")
-    if (lastTurn === "a") {
-      alert("you passed everyone and you won the race")
-    } else if (lastTurn === "b") {
-      alert("you crashed lost the game ")
-    } else if (lastTurn === "c") {
-      alert("unfortunately you lost some speed and finshed the game last ")
-
-  }
-  }else if (powerUp==="b"){
-    alert("you have crashed please try again ")
-  }
-}
-function sameSpeed (){
-  if (maintainSpeed==="a"){
-    alert("your are going too fast and you crash ")
-
-  }else if (maintainSpeed==="b"){
-    alert("you got through the turn and you are first place and you won the race  ")
-
-  }else if (maintainSpeed ==="c")
-  {
-
-    alert("you made through the turn and you won the race ")
-  }
-  
-
-}
-// this is fot the toyota\
-function toyotaturnning(){
-  if (powerUp === "a") {
-    alert("you got an handling upgrade ");
-     lastTurn = prompt("you are at the last turn do you A. maintain your speed B. speed up C. slow down  ")
-    if (lastTurn === "a") {
-      alert("you completed the turning but unfortunatly you ended the game at 4th because you are too slow")
-    } else if (lastTurn === "b") {
-      alert("you completed the turning and won the game")
-    } else if (lastTurn === "c") {
-      alert("  you completed the turning but unfortunatly you ended the game at 3rd")
-  
-  
-  }
-}else if (powerUp==="b"){
-  alert("you landed in front of the turning and won the game  ")
-}
-}
-function ToyotasameSpeed (){
-  if (maintainSpeed==="a"){
-    alert("you made it throught the turn and won the game  ")
-
-  }else if (maintainSpeed==="b"){
-    alert("you got through the turn but you finished the race at 4th  ")
-
-  }else if (maintainSpeed ==="c"){
-
-    alert("you made through the turn  but you finshed the race as 2nd  ")
-  }
-
-  
-
 }
